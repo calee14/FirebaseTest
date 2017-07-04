deaf6ad45cfcb2cb6f80e68583526c8b4834a2bd
calee14 <caplee1000@gmail.com>
2017-07-04 14:55:55 -0700

    GamesFirebase

--- FirebaseTest/GameScene.swift
+++ FirebaseTest/GameScene.swift
@@ -8,82 +8,71 @@
 
 import SpriteKit
 import GameplayKit
+import Firebase
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ref: FIRDatabaseReference!
+    var currentScore: Int = 26
+    var highscore1: String!
+    var highscore2: String!
+    var highscore3: String!
     
     override func didMove(to view: SKView) {
+        ref = FIRDatabase.database().reference()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        FIRAuth.auth()?.signIn(withEmail: "theonlyuser@gmail.com", password: "123456", completion: { (user, error) in
+            let email = user?.email
+            print(email!)
+        })
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        //Push data
+        //ref.child("highscores").setValue(["highscore1": "3134", "highscore2": "1234", "highscore3": "21"])
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //Pull data
+        ref.child("highscores").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            self.highscore1 = snapshot.childSnapshot(forPath: "highscore1").value as! String
+            self.highscore2 = snapshot.childSnapshot(forPath: "highscore2").value as! String
+            self.highscore3 = snapshot.childSnapshot(forPath: "highscore3").value as! String
+            
+            print(self.highscore1)
+            print(self.highscore2)
+            print(self.highscore3)
+            
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func checkIfNewHigh(num: Int) {
+        guard let highscore1 = highscore1 else {
+            return
+        }
+        guard let highscore2 = highscore2 else {
+            return
+        }
+        guard let highscore3 = highscore3 else {
+            return
+        }
+        if num > Int(highscore1)! {
+            print(num)
+            ref.child("highscores").child("highscore1").setValue(String(num))
+        } else if num > Int(highscore2)! {
+            print(num)
+            ref.child("highscores").child("highscore2").setValue(String(num))
+        } else if num > Int(highscore3)! {
+            print(num)
+            ref.child("highscores").child("highscore3").setValue(String(num))
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        checkIfNewHigh(num: currentScore)
     }
 }
